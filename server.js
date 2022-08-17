e9349ecd11212d38da95c5d18c2c6671931caceb
Alex Johnson <johnsonav1992@gmail.com>
2022-08-16 20:29:57 -0500

    express.static update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static('cocktail-app'))
+app.use(express.static(root))
 
 ///Routes///
 require(`./server/routes.js`)(app)
