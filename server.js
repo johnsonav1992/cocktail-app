c8b296467cb7930dadf85b11d7fa26ed2ca30be8
Alex Johnson <johnsonav1992@gmail.com>
2022-08-16 21:01:50 -0500

    maybe this will deploy...

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('client'))
 
-const controller = require('../cocktail-app/server/controller.js');
+const controller = require('./server/controller.js');
 const { getHomePage } = controller;
 
 ///Routes///
