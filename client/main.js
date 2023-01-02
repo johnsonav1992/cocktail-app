22eacd124b703005e13210e2e3f13602f2f8965e
Alex Johnson <97169070+johnsonav1992@users.noreply.github.com>
2023-01-02 10:20:12 -0600

    Change auto unit loading drink

--- client/main.js
+++ client/main.js
@@ -15,7 +15,7 @@ getRandomCocktail()
 
 async function initDisplay() {
 	try {
-		displayCocktail((await axios.get(`/drinks/letter/m`)).data, 15)
+		displayCocktail((await axios.get(`/drinks/letter/a`)).data, 15)
 	} catch (err) {
 		console.log(err)
 	}
